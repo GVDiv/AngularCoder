bbd7d9aedf8b52b0632333b71aff7d3df213f595
Gabriel Villamayor <gabitokun@outlook.com>
2022-10-27 20:44:18 -0300

    add inicio links

--- src/app/core/components/inicio/inicio.component.ts
+++ src/app/core/components/inicio/inicio.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Sesion } from 'src/app/models/sesion';
+import { SesionService } from '../../services/sesion.service';
 
 @Component({
   selector: 'app-inicio',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
-
-  constructor() { }
+  sesion$!: Observable<Sesion>;
+  
+  constructor(
+    private sesionService: SesionService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.sesion$ = this.sesionService.obtenerSesion();
+  }
+
+  verCursos(){
+    this.router.navigate(['/cursos/listar']);
+  }
+
+  verAlumnos(){
+    this.router.navigate(['/alumnos/listar']);
   }
 
 }
